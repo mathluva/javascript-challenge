6fd6014ccc6530666c50f09e53eac45ab6211342
mathluva <mathluvacoding@gmail.com>
2020-09-09 20:30:17 -0400

    update index

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,23 @@
 // from data.js
 var tableData = data;
+//reference the table body from html file
+var tbody = d3.select("tbody")
 
-// YOUR CODE HERE!
+// Console.log the  data from data.js
+    console.log(data);
+
+//Use d3 to update each cell's text with values 
+
+data.forEach(function(ufodata) {
+    console.log(ufodata);
+    var row = tbody.append("tr");
+    
+    Object.entries(ufodata).forEach(function([key, value]) {
+   console.log(key, value);
+
+   // Append a cell to the row for each value
+
+var cell = row.append("td");
+    cell.text(value);
+ });
+});
