1a696346856329de7459974f4ed8b318cceb0294
mathluva <mathluvacoding@gmail.com>
2020-09-09 21:28:53 -0400

    filter table

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
 // from data.js
 var tableData = data;
-//reference the table body from html file
+
 var tbody = d3.select("tbody")
 
 // Console.log the  data from data.js
@@ -21,3 +21,37 @@ var cell = row.append("td");
     cell.text(value);
  });
 });
+// Select the button
+var button = d3.select("#button");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+// Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(i => i.datetime === inputValue);
+
+console.log(filteredData);
+var list = d3.select(".summary");
+
+
+  // append stats to the list
+  list.append(filteredData);
+};
